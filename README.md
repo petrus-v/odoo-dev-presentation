22e95ed12689e9035559e009ba8693420b1255e2
Pierre Verkest <pierre@verkest.fr>
2026-09-15 00:10:43 +0200

    Step 1: Install Odoo with a custom module

---
+++ README.md
@@ -0,0 +1,11 @@
+# OCA Days 2026: Odoo dev environement
+
+## Running the project
+
+```bash
+uv run odoo  \
+  -d ocadays26 -i ocadays26 \
+  --stop-after-init \
+  --no-http \
+  --without-demo=True
+```
---
+++ src/odoo/addons/ocadays26/__manifest__.py
@@ -0,0 +1,10 @@
+{
+  "name": "OCA Days 2026",
+  "version": "19.0.1.0.0",
+  "summary": "OCA Days 2026 - odoo dev module",
+  "depends": [
+    "web",
+  ],
+  "author": "Pierre Verkest <pierre.verkest@apycod.fr>",
+  "license": "AGPL-3",
+}
