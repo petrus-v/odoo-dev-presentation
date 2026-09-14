dfca068272f990622fa0b7e303b821f484e9e879
Pierre Verkest <pierre@verkest.fr>
2026-09-15 01:08:27 +0200

    Step 3: Add mis_builder an OCA dependency

--- src/odoo/addons/ocadays26/__manifest__.py
+++ src/odoo/addons/ocadays26/__manifest__.py
@@ -4,6 +4,7 @@
   "summary": "OCA Days 2026 - odoo dev module",
   "depends": [
     "web",
+    "mis_builder",
   ],
   "author": "Pierre Verkest <pierre.verkest@apycod.fr>",
   "license": "AGPL-3",
